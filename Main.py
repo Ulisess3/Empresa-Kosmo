362ce4cd6929d968c534894b7cd4dd94a666f22e
Ulisess3 <nicolasulises21@gmail.com>
2024-05-15 22:58:39 -0400

    Avance 1

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ letra = Font(family="Bold", size=25)
 top = Frame(root, bg="gray", width=600, height=60).place(x=0, y=0)
 
 Button(top, text="Iniciar Sesión").place(x=500, y=18)
-Button(top, text="Registrase").place(x=420, y=18)
+Button(top, text="Registrarse").place(x=420, y=18)
 
 Label(top, text="KOSMOS", bg="gray", font=letra).place(x=30, y=10)
 Label(root, text="*aquí estarán los productos*").place(x=250, y=200)
