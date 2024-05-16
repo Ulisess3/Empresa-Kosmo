0e85610a3b61e834fe0b065787b0d33b68410e05
Ulisess3 <nicolasulises21@gmail.com>
2024-05-15 22:53:15 -0400

    avance 1

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.font import Font
 
 root = Tk()
 
@@ -6,8 +7,14 @@ root.title("Empresa Kosmos")
 root.geometry("600x400")
 root.config(bg="lightgray")
 root.resizable(0,0)
+letra = Font(family="Bold", size=25)
 
 top = Frame(root, bg="gray", width=600, height=60).place(x=0, y=0)
+
 Button(top, text="Iniciar Sesión").place(x=500, y=18)
+Button(top, text="Registrase").place(x=420, y=18)
+
+Label(top, text="KOSMOS", bg="gray", font=letra).place(x=30, y=10)
+Label(root, text="*aquí estarán los productos*").place(x=250, y=200)
 
 root.mainloop()
