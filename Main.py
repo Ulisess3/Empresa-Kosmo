599bf77e93e98b8f148b33505d14a7d146750878
Ulisess3 <nicolasulises21@gmail.com>
2024-05-15 22:13:33 -0400

    avance algo en main

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-#???
+from tkinter import *
+
+root = Tk()
+
+root.title("Empresa Kosmos")
+root.geometry("600x400")
+root.config(bg="lightgray")
+root.resizable(0,0)
+
+top = Frame(root, bg="gray", width=600, height=60).place(x=0, y=0)
+Button(top, text="Iniciar Sesión").place(x=500, y=18)
+
+root.mainloop()
